a3a98b8b38e6609fd9669dc09800827863eda1d7
Raghvendra Jain <raghavendra.jain@gmail.com>
2019-03-26 14:35:59 +0900

    Simple flask app added

---
+++ simple_flask_app/hello.py
@@ -0,0 +1,17 @@
+
+from flask import Flask, render_template 
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/greeting/<int:planet>")
+def hello(planet):
+    return render_template("hello.html", t_planet = planet)
+
+
+
+
